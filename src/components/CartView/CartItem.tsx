93e24c68a7d2f9d745b271e7f7f0eacfa3ca20b2
AnnaPravdiukova <annagekata82@gmail.com>
2023-12-21 20:51:47 +0200

    redesigned cart items

--- src/components/CartView/CartItem.tsx
+++ src/components/CartView/CartItem.tsx
@@ -1,60 +1,49 @@
-import { MinusIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 import { DocumentData } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
 import { useCart } from '../../contexts/CartContext'
 import { formattedPrice } from '../../utils/helpers'
 import { useNavigate } from 'react-router-dom'
+import { Counter } from './Counter'
 
 interface CartItemProps {
   product: DocumentData
 }
 
 export const CartItem = ({ product }: CartItemProps) => {
-  console.log('CartItem [product]', product)
-
-  const { removeFromCart, changeProductQuantity } = useCart()
+  const { removeFromCart } = useCart()
   const { category, productId, description: { photo, price }, title, quantity } = product
-  const [currentPrice, setCurrentPrice] = useState<number>(price)
   const navigate = useNavigate()
-
-  const add = () => changeProductQuantity(productId, quantity + 1)
-
-  const subtract = () => {
-    if (quantity > 1) return changeProductQuantity(productId, quantity - 1)
-  }
-
   const remove = () => removeFromCart(productId)
-
   const showProductPage = () => navigate(`/${category}/${productId}`)
 
-  useEffect(() => {
-    setCurrentPrice(price * quantity)
-  }, [quantity])
-
   return (
-    <div className='flex flex-row flex-wrap md:flex-nowrap justify-between gap-4 p-3 border border-collapse place-items-center'>
-      <div className='flex w-full grow gap-x-2 sm:gap-x-4 place-items-center cursor-pointer hover:bg-base-300 transition-colors'>
-        <figure className='shrink-0'>
-          <img src={photo} alt={title} className='h-8 sm:h-16'></img>
-        </figure>
-        <div className='grow px-2' onClick={showProductPage}>{title}</div>
-      </div>
-      <div className='shrink-0 px-2 ms-auto'>{quantity} pcs</div>
-      <div className='shrink-0 px-2'>{formattedPrice(currentPrice)} hrn</div>
-      <div className='flex gap-x-2 sm:gap-x-4 place-items-center ms-auto'>
-        <div className='flex flex-row gap-x-2 justify-between place-items-center min-w-[7.5rem] shrink-0'>
-          <button className='btn btn-square btn-sm shrink-0' onClick={subtract}>
-            <MinusIcon className='w-4 h-4 sm:w-5 sm:h-5' />
-          </button>
-          <div className='px-2 grow text-center'>{quantity}</div>
-          <button className='btn btn-square btn-sm shrink-0' onClick={add}>
-            <PlusIcon className='w-4 h-4 sm:w-5 sm:h-5' />
-          </button>
+    <div className='flex flex-row flex-nowrap gap-x-3 p-3 border'>
+      <figure className='shrink-0'>
+        <img src={photo} alt={title} className='h-16'></img>
+      </figure>
+      <div className='grow flex flex-row w-full justify-between gap-2.5'>
+        <div className='flex flex-col md:flex-row gap-2.5 grow'>
+          <div
+            className='flex flex-col gap-2 grow group transition-colors cursor-pointer'
+            onClick={showProductPage}
+          >
+            <div className='group-hover:underline group-hover:underline-offset-4 text-lg'>
+              {title}
+            </div>
+            <div className='font-bold'>{formattedPrice(price)} hrn</div>
+          </div>
+          <Counter quantity={quantity} productId={productId} />
         </div>
-        <div className='shrink-0 place-center sm:ms-4'>
-          <button className='btn btn-square btn-sm' onClick={remove}>
-            <TrashIcon className='w-4 h-4 sm:w-5 sm:h-5' />
+        <div className='flex flex-col justify-between md:flex-row-reverse md:justify-normal gap-5 min-w-[25%]'>
+          <button
+            className='btn btn-square btn-ghost btn-sm self-end md:self-start'
+            onClick={remove}
+          >
+            <XMarkIcon className='w-4 h-4 sm:w-5 sm:h-5' />
           </button>
+          <div className='font-medium text-end md:place-self-center'>
+            {formattedPrice(price * quantity)} hrn
+          </div>
         </div>
       </div>
     </div>
--- src/components/CartView/TotalPrice.tsx
+++ src/components/CartView/TotalPrice.tsx
@@ -5,13 +5,14 @@ type TotalPriceProps = {
   totalItems: number
 }
 
-export const TotalPrice = ({ totalPrice }: TotalPriceProps) => {
+export const TotalPrice = ({ totalPrice, totalItems }: TotalPriceProps) => {
   const formattedTotalPrice = formattedPrice(totalPrice)
 
   return (
     <div className='flex flex-row justify-between items-baseline gap-5 p-2 w-full max-w-xs ms-auto border bg-neutral-50'>
-      <div>Total:</div>
-      <div>{formattedTotalPrice}</div>
+      <div className='font-semibold'>Total:</div>
+      <div>{totalItems} items</div>
+      <div><b>{formattedTotalPrice}</b> hrn</div>
     </div>
   )
 }
