c74651ac6bc4964b7e4028a8cdf6225c96c5e8ff
AnnaPravdiukova <annagekata82@gmail.com>
2024-01-19 16:08:44 +0200

    added fields to product page

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -4,6 +4,7 @@ import { DocumentData, doc, getDoc } from "firebase/firestore"
 import { formattedPrice } from "../utils/helpers"
 import { useCart } from "../contexts/CartContext"
 import { StaticRatingComponent } from "../components/RatingComponent/StaticRating"
+import { ProductDetails } from "../components/ProductPage/ProductDetails"
 
 export const getCurrentProduct = async ({ params }: any) => {
   const { productId, category } = params
@@ -34,12 +35,11 @@ export const ProductPage = () => {
   console.log('product', product, category)
   return (
     <div className='px-5 container mx-auto'>
-      {/* category {category}, product {product?.title} */}
-      <div className='grid grid-cols-1 md:grid-cols-3 py-5 gap-x-5 gap-y-10 items-stretch'>
-        <div className='w-96 h-96 place-self-center md:w-full md:h-full md:place-self-start'>
+      <div className='grid grid-cols-1 md:grid-cols-[1fr,_2fr] py-5 gap-x-5 gap-y-10 items-stretch'>
+        <div className='w-96 h-96 place-self-center md:w-full md:h-full md:place-self-start overflow-clip'>
           <figure><img src={product?.description?.photo[0]} alt={product?.title} /></figure>
         </div>
-        <div className='md:col-span-2 flex flex-col gap-y-5'>
+        <div className='flex flex-col gap-y-5'>
           <h1 className='text-3xl font-semibold text-center'>{product?.title}</h1>
           <p className='text-lg font-medium text-base-content text-opacity-50'>{product?.description?.subtitle}</p>
           <div className='text-3xl font-semibold'>{formattedPrice(product?.description?.price)}</div>
@@ -50,13 +50,12 @@ export const ProductPage = () => {
             <button className='btn btn-outline' onClick={addToCartHelper}>
               Add to cart
             </button>
-            <button className='btn btn-primary'>
-              Buy now!
-            </button>
           </div>
         </div>
-        <div>Components:</div>
-        <div className='px-5 border-s border-primary md:col-span-2'>
+        <aside>
+          <ProductDetails product={product} category={category} />
+        </aside>
+        <div className='px-5 md:border-s border-primary'>
           <p className='text-base font-light'>{product?.description?.mainText}</p>
         </div>
       </div>
