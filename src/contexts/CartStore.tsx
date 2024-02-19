7adb01a067489495b673a9f7c8829f82d984852f
Anna Pravdiukova <annagekata82@gmail.com>
2024-02-19 20:37:32 +0200

    added cart api, added few methods to cart store

--- src/contexts/CartStore.tsx
+++ src/contexts/CartStore.tsx
@@ -1,6 +1,9 @@
 import { DocumentData, DocumentReference } from 'firebase/firestore'
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
+import { useAuthStore } from './AuthStore'
+import { head, get, pick } from 'lodash'
+import { addNewProductToCart, getCartData, changeProductQuantity, updateList } from './CartApiUtils'
 
 export type CartProductType = {
   productId: string
@@ -14,36 +17,68 @@ interface CartStore {
   totalItems: number
   setProducts: (products: CartProductType[]) => void
   setTotalItems: (totalItems: number) => void
-  // clearCart: () => void
-  // addToCart: (productId: string, productData: DocumentData) => void
+  clearCart: () => void
+  addToCart: (productId: string, productData: DocumentData) => void
   // removeFromCart: (productId: string) => void
   // updateList: (props: CartProductType[]) => void
   // changeProductQuantity: (productId: string, quantity: number) => void
   // getProductQuantity: (productId: string) => number
 }
 
+export interface IMinProductData {
+  price: number
+  photo: string[]
+  title: string
+  productId: string
+  category: string
+}
+
+export const cartInitialState = {
+  products: [],
+  totalItems: 0,
+}
+const minProductData = (productData: DocumentData) => ({
+  ...pick(productData, ['title', 'productId', 'category']),
+  price: get(productData, ['description', 'price'], 0),
+  photo: head(get(productData,  ['description', 'photo'], []))
+}) as IMinProductData
+
+const currentUserUID = useAuthStore.getState().currentUser?.uid;
+
 export const useCartStore = create<CartStore>()(
   persist(
-    (set) => ({
-      products: [],
-      totalItems: 0,
+    (set, get) => ({
+      ...cartInitialState,
       setProducts: (products) => set({ products }),
       setTotalItems: (totalItems) => set({ totalItems }),
+      clearCart: async () => {
+        if (!currentUserUID) return 'Unauthorized'
 
-      // logout: async () => {
-      //   try {
-      //     await signOut(auth);
-      //     return set({ currentUser: null });
-      //   } catch (error) {
-      //     return console.log('SignOut [error]:', error);
-      //   }
-      // },
+        await updateList(currentUserUID, [])
+        set(cartInitialState)
+      },
+      addToCart: async (productId, productData) => {
+        if (!currentUserUID) return 'Unauthorized'
+
+        const products = get().products
+        const indexProduct = products.findIndex(
+          ({ productId: id }) => id === productId
+        )
+        const isNewProduct = indexProduct === -1
+        if (isNewProduct) {
+          await addNewProductToCart(currentUserUID, products, productId, minProductData(productData))
+        } else {
+          await changeProductQuantity(currentUserUID, products, productId, products[indexProduct]?.quantity + 1)
+        }
+        const updatedValue = await getCartData(currentUserUID)
+        if (updatedValue !== undefined) set(updatedValue)
+      },
     }),
     {
       name: 'cart-storage', // name of the item in the storage (must be unique)
       storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
       partialize: (state) => ({
-        currentUser: state.products,
+        products: state.products,
         totalItems: state.totalItems
       }),
     }
